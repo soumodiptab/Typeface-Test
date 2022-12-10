bc6527d443b0fea7b4690fb0ce8f06c93388b4c9
Soumodipta Bose <sbose2019@gmail.com>
2022-12-10 23:25:51 +0530

    commit 2

--- 2.py
+++ 2.py
@@ -1,33 +1,20 @@
-import phonetics
+# pip install fuzzywuzzy
+# pip install python-Levenshtein
 
+from fuzzywuzzy import process
+from fuzzywuzzy import fuzz
+sol = []
 
-def get_phonetic(s):
-    return phonetics.dmetaphone(s)
+inp = input("Enter the phonetic word: ")
+file = input("Enter the name of the file: ")
 
+f = open(file, 'r')
 
-phonet_list = []
-lines = []
-outfile = "match.txt"
-data = input("Enter match strings separated by space : ").split(" ")
-filename = input("Enter name of the file: ")
-out = open(outfile, "w")
+for line in f.readlines():
+    word = line.rstrip('\n')
 
+    matchval = fuzz.ratio(inp, word)
+    if (matchval >= 50):
+        sol.append(word)
 
-with open(filename) as f:
-    lines = [line.rstrip('\n') for line in f]
-
-for word in lines:
-    phonet_list.append(get_phonetic(word))
-
-for word in data:
-    ans = []
-    ret_val = get_phonetic(word)
-    for val in ret_val:
-        for i in range(len(lines)):
-            lis = phonet_list[i]
-            w = lines[i]
-            if val != '' and val in lis:
-                ans.append(w)
-                break
-    res = " ".join(ans)
-    out.write(res)
+print(sol)
